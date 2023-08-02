a84b4d9fb96f775233b5ebf6032f3a55b92c04d5
shooshoo <shilan_hk@yahoo.com>
2023-08-02 16:39:38 -0400

    added testing action message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("testing action");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined!");
   }
